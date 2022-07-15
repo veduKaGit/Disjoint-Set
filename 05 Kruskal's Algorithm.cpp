e52d6aeefa67d15668f9ea636d627f2ae6ebb5cd
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-07-15 23:58:14 +0530

    Update 05 Kruskal's Algorithm.cpp

--- 05 Kruskal's Algorithm.cpp
+++ 05 Kruskal's Algorithm.cpp
@@ -11,197 +11,161 @@
 // The Greedy Choice is to pick the smallest weight edge that does not cause a cycle in the MST(min spanning tree) constructed so far. 
 
 
-	
-// C++ program for Kruskal's algorithm to find Minimum Spanning Tree 
-// of a given connected, undirected and weighted graph 
-#include <bits/stdc++.h> 
-using namespace std; 
 
-// a structure to represent a weighted edge in graph 
-class Edge 
-{ 
-	public: 
-	int src, dest, weight; 
-}; 
 
-// a structure to represent a connected, undirected 
-// and weighted graph 
-class Graph 
-{ 
-	public: 
-	// V-> Number of vertices, E-> Number of edges 
-	int V, E; 
+#include<bits/stdc++.h>
+using namespace std;
 
-	// graph is represented as an array of edges. 
-	// Since the graph is undirected, the edge 
-	// from src to dest is also edge from dest 
-	// to src. Both are counted as 1 edge here. 
-	Edge* edge; 
-}; 
+// Creating shortcut for an integer pair
+typedef pair<int, int> iPair;
 
-// Creates a graph with V vertices and E edges 
-Graph* createGraph(int V, int E) 
-{ 
-	Graph* graph = new Graph; 
-	graph->V = V; 
-	graph->E = E; 
+// Structure to represent a graph
+struct Graph
+{
+	int V, E;
+	vector< pair<int, iPair> > edges;
 
-	graph->edge = new Edge[E]; 
+	// Constructor
+	Graph(int V, int E)
+	{
+		this->V = V;
+		this->E = E;
+	}
 
-	return graph; 
-} 
+	// Utility function to add an edge
+	void addEdge(int u, int v, int w)
+	{
+		edges.push_back({w, {u, v}});
+	}
 
-// A structure to represent a subset for union-find 
-class subset 
-{ 
-	public: 
-	int parent; 
-	int rank; 
-}; 
+	// Function to find MST using Kruskal's
+	// MST algorithm
+	int kruskalMST();
+};
 
-// A utility function to find set of an element i 
-// (uses path compression technique) 
-int find(subset subsets[], int i) 
-{ 
-	// find root and make root as parent of i 
-	// (path compression) 
-	if (subsets[i].parent != i) 
-		subsets[i].parent = find(subsets, subsets[i].parent); 
+// To represent Disjoint Sets
+struct DisjointSets
+{
+	int *parent, *rnk;
+	int n;
 
-	return subsets[i].parent; 
-} 
+	// Constructor.
+	DisjointSets(int n)
+	{
+		// Allocate memory
+		this->n = n;
+		parent = new int[n+1];
+		rnk = new int[n+1];
 
-// A function that does union of two sets of x and y 
-// (uses union by rank) 
-void Union(subset subsets[], int x, int y) 
-{ 
-	int xroot = find(subsets, x); 
-	int yroot = find(subsets, y); 
+		// Initially, all vertices are in
+		// different sets and have rank 0.
+		for (int i = 0; i <= n; i++)
+		{
+			rnk[i] = 0;
 
-	// Attach smaller rank tree under root of high 
-	// rank tree (Union by Rank) 
-	if (subsets[xroot].rank < subsets[yroot].rank) 
-		subsets[xroot].parent = yroot; 
-	else if (subsets[xroot].rank > subsets[yroot].rank) 
-		subsets[yroot].parent = xroot; 
+			//every element is parent of itself
+			parent[i] = i;
+		}
+	}
 
-	// If ranks are same, then make one as root and 
-	// increment its rank by one 
-	else
-	{ 
-		subsets[yroot].parent = xroot; 
-		subsets[xroot].rank++; 
-	} 
-} 
+	// Find the parent of a node 'u'
+	// Path Compression
+	int find(int u)
+	{
+		/* Make the parent of the nodes in the path
+		from u--> parent[u] point to parent[u] */
+		if (u != parent[u])
+			parent[u] = find(parent[u]);
+		return parent[u];
+	}
 
-// Compare two edges according to their weights. 
-// Used in qsort() for sorting an array of edges 
-int myComp(const void* a, const void* b) 
-{ 
-	Edge* a1 = (Edge*)a; 
-	Edge* b1 = (Edge*)b; 
-	return a1->weight > b1->weight; 
-} 
+	// Union by rank
+	void merge(int x, int y)
+	{
+		x = find(x), y = find(y);
 
-// The main function to construct MST using Kruskal's algorithm 
-void KruskalMST(Graph* graph) 
-{ 
-	int V = graph->V; 
-	Edge result[V]; // Tnis will store the resultant MST 
-	int e = 0; // An index variable, used for result[] 
-	int i = 0; // An index variable, used for sorted edges 
+		/* Make tree with smaller height
+		a subtree of the other tree */
+		if (rnk[x] > rnk[y])
+			parent[y] = x;
+		else // If rnk[x] <= rnk[y]
+			parent[x] = y;
 
-	// Step 1: Sort all the edges in non-decreasing 
-	// order of their weight. If we are not allowed to 
-	// change the given graph, we can create a copy of 
-	// array of edges 
-	qsort(graph->edge, graph->E, sizeof(graph->edge[0]), myComp); 
+		if (rnk[x] == rnk[y])
+			rnk[y]++;
+	}
+};
 
-	// Allocate memory for creating V ssubsets 
-	subset *subsets = new subset[( V * sizeof(subset) )]; 
+/* Functions returns weight of the MST*/
 
-	// Create V subsets with single elements 
-	for (int v = 0; v < V; ++v) 
-	{ 
-		subsets[v].parent = v; 
-		subsets[v].rank = 0; 
-	} 
-    
-    int res =0;
-	// Number of edges to be taken is equal to V-1 
-	while (e < V - 1 && i < graph->E) 
-	{ 
-		// Step 2: Pick the smallest edge. And increment 
-		// the index for next iteration 
-		Edge next_edge = graph->edge[i++]; 
+int Graph::kruskalMST()
+{
+	int mst_wt = 0; // Initialize result
 
-		int x = find(subsets, next_edge.src); 
-		int y = find(subsets, next_edge.dest); 
+	// Sort edges in increasing order on basis of cost
+	sort(edges.begin(), edges.end());
 
-		// If including this edge does't cause cycle, 
-		// include it in result and increment the index 
-		// of result for next edge 
-		if (x != y) 
-		{ 
-			result[e++] = next_edge; 
-			Union(subsets, x, y); 
-			res+=next_edge.weight;
-		} 
-		// Else discard the next_edge 
-	} 
+	// Create disjoint sets
+	DisjointSets ds(V);
 
-	// print the contents of result[] to display the 
-	// built MST 
-	
-	cout<<"Weight of MST is: "<<res<<endl;
-	return; 
-} 
+	// Iterate through all sorted edges
+	vector< pair<int, iPair> >::iterator it;
+	for (it=edges.begin(); it!=edges.end(); it++)
+	{
+		int u = it->second.first;
+		int v = it->second.second;
 
-// Driver code 
-int main() 
-{ 
-        int V = 5; // Number of vertices in graph 
-		int E = 7; // Number of edges in graph 
-		Graph* graph = createGraph(V, E);
+		int set_u = ds.find(u);
+		int set_v = ds.find(v);
 
-		// add edge 0-1 
-		graph->edge[0].src = 0; 
-		graph->edge[0].dest = 1; 
-		graph->edge[0].weight = 10; 
+		// Check if the selected edge is creating
+		// a cycle or not (Cycle is created if u
+		// and v belong to same set)
+		if (set_u != set_v)
+		{
+			// Current edge will be in the MST
+			// so print it
+			cout << u << " - " << v << endl;
 
-		// add edge 0-2 
-		graph->edge[1].src = 0; 
-		graph->edge[1].dest = 2; 
-		graph->edge[1].weight = 8; 
+			// Update MST weight
+			mst_wt += it->first;
 
-		// add edge 0-3 
-		graph->edge[2].src = 1; 
-		graph->edge[2].dest = 2; 
-		graph->edge[2].weight = 5; 
+			// Merge two sets
+			ds.merge(set_u, set_v);
+		}
+	}
 
-		// add edge 1-3 
-		graph->edge[3].src = 1; 
-		graph->edge[3].dest = 3; 
-		graph->edge[3].weight = 3; 
+	return mst_wt;
+}
 
-		// add edge 2-3 
-		graph->edge[4].src = 2; 
-		graph->edge[4].dest = 3; 
-		graph->edge[4].weight = 4; 
-		
-		//add egde 2-4
-		graph->edge[5].src = 2; 
-		graph->edge[5].dest = 4; 
-		graph->edge[5].weight = 12;
-		
-		// add edge 3-4
-		graph->edge[6].src = 3; 
-		graph->edge[6].dest = 4; 
-		graph->edge[6].weight = 15; 
+// Driver program to test above functions
+int main()
+{
+	/* Let us create above shown weighted
+	and undirected graph */
+	int V = 9, E = 14;
+	Graph g(V, E);
 
-	KruskalMST(graph); 
+	// making above shown graph
+	g.addEdge(0, 1, 4);
+	g.addEdge(0, 7, 8);
+	g.addEdge(1, 2, 8);
+	g.addEdge(1, 7, 11);
+	g.addEdge(2, 3, 7);
+	g.addEdge(2, 8, 2);
+	g.addEdge(2, 5, 4);
+	g.addEdge(3, 4, 9);
+	g.addEdge(3, 5, 14);
+	g.addEdge(4, 5, 10);
+	g.addEdge(5, 6, 2);
+	g.addEdge(6, 7, 1);
+	g.addEdge(6, 8, 6);
+	g.addEdge(7, 8, 7);
 
-	return 0; 
-} 
+	cout << "Edges of MST are \n";
+	int mst_wt = g.kruskalMST();
 
-// This code is contributed by rathbhupendra 
+	cout << "\nWeight of MST is " << mst_wt;
+
+	return 0;
+}
