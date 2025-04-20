30872f4a1b020005df504adea8d29e459d7d6adc
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2025-04-20 13:50:30 +0530

    Update and rename 04 time_complexity.md to 04 time_complexity.cpp

---
+++ 04 time_complexity.cpp
@@ -0,0 +1,8 @@
+// Time complexity of both ***FIND*** and ***UNION*** (since union func uses find func in it) operations = O(Log N). 
+
+// The two techniques (union by rank + path compression) complement each other. 
+// When we use both of them together:
+
+1. The time complexity of both ***FIND*** and ***UNION*** (since union func uses find func in it) operations becomes = O(1) => VVIMP!!!!
+// 2. O(1) (amortized) or O(α(n)), where α(n) is the inverse Ackermann function.
+// 3. In practical terms, α(n) grows so slowly that it's effectively a constant, typically less than 5. 
--- 04 time_complexity.md
+++
@@ -1,5 +0,0 @@
-The two techniques (union by rank + path compression) complement each other. 
-
-When we use both of them together:
-1. The time complexity of both ***FIND*** and ***UNION*** (since union func uses find func in it) operations becomes even SMALLER than O(Log N). 
-2. In fact, AMORTIZED time complexity effectively becomes small CONSTANT. 
